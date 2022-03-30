4ad67fcba80372faffa18347bb2b6a65a48ba62b
Viktoriia Fedynaik <xfedyniak@stuba.sk>
2022-03-30 15:42:41 +0200

    close frame with buttton

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,10 +1,12 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.utility.Okno;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Clovek c = new Clovek();
+       new Okno();
 
     }
 }
